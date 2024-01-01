002471f4712e0f4ee6a677bd8f5c6e3245dfae9c
Rfusar <rfusaro12@gmail.com>
2024-01-01 22:17:31 +0100

    0.0.0

--- apps/code/home_page/corsi/linguaggi.cpp
+++ apps/code/home_page/corsi/linguaggi.cpp
@@ -6,6 +6,7 @@ vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> LINGUAGGI(
 
     linguaggi.link = {
         {"GUARDA","cls && type C:\\Users\\Utente\\Desktop\\Superhero\\apps\\code\\home_page\\corsi.txt"},
+        {"MODIFICA","notepad C:\\Users\\Utente\\Desktop\\Superhero\\apps\\code\\home_page\\corsi.txt"},
     };
     
     vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> Menu = {
