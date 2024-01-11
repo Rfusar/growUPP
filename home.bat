d28fbfe5260b912ebea7087cae74e8271c0849aa
Rfusar <rfusaro12@gmail.com>
2024-01-11 16:23:18 +0100

    0.0.0

--- home.bat
+++ home.bat
@@ -1,18 +1,21 @@
 @echo off
 
 if "%1" == "update" ( 
-    cd C:\Users\Utente\Desktop\Superhero\HOME_STUDIO
+    cd C:\Users\Utente\Desktop\Superhero
     code .
 
 ) else if "%1" == "start" ( 
-    cd HOME_STUDIO
-    .\home_page.exe
-    cd ..
+    C:\Users\Utente\Desktop\Superhero\HOME_STUDIO\home_page.exe
+
 
 ) else if "%1" == "github" (
      cd C:\Users\Utente\Desktop\Superhero
     git add .
     git commit -m "0.0.0"
     git push origin master
-    
+
+
+) else if "%1" == "workout" (
+    cls
+    type C:\Users\Utente\Desktop\Superhero\cose_da_fare.txt
 )
