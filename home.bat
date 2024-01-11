39c4fed5baa268a95e04bb86687a3b1d16da1453
Rfusar <rfusaro12@gmail.com>
2024-01-11 08:59:56 +0100

    0.0.0

--- home.bat
+++ home.bat
@@ -8,9 +8,11 @@ if "%1" == "update" (
     cd HOME_STUDIO
     .\home_page.exe
     cd ..
-    
+
 ) else if "%1" == "github" (
+     cd C:\Users\Utente\Desktop\Superhero
     git add .
     git commit -m "0.0.0"
     git push origin master
+    
 )
