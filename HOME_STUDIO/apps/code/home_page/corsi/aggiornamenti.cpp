822761cc6e185f5639b94f69fb9f39d10c3adb31
Rfusar <rfusaro12@gmail.com>
2024-03-12 07:47:04 +0100

    0.0.0

--- HOME_STUDIO/apps/code/home_page/corsi/aggiornamenti.cpp
+++ HOME_STUDIO/apps/code/home_page/corsi/aggiornamenti.cpp
@@ -1,6 +1,6 @@
 #include "..\home_page.hpp"
 
-Dataset aggiornamenti, nuovo;
+Dataset aggiornamenti;
 
 vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> AGGIORNAMENTI(){
     aggiornamenti.link = {
@@ -14,37 +14,9 @@ vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> AGGIORNAME
             "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp\\subnet && "
             "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp\\subnet"},
     };
-    nuovo.link = {
-        {"Batch", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Batch && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Batch"},
-        {"Cpp", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp"},
-        {"Java", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Java && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Java"},
-        {"Zig", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Zig && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Zig"},
-        {"NodeJS", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_NodeJS && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_NodeJS"},
-        {"Go", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Go && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Go"},
-        {"Python", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Python && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Python"},
-        {"C#", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_C# && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_C#"},
-        {"Web", 
-            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Web && "
-            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Web"},
-    };
+    
     vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> Menu = {
-        {"AGGIORAMENTI", { {"Crea programma","Aggiorna"}, {nuovo, aggiornamenti} } }
+        {"AGGIORNAMENTI", { {"Aggiorna"}, {aggiornamenti} } }
     };
     
     return Menu;
--- HOME_STUDIO/apps/code/home_page/corsi/programmi.cpp
+++ HOME_STUDIO/apps/code/home_page/corsi/programmi.cpp
@@ -1,6 +1,6 @@
 #include "..\home_page.hpp"
 
-Dataset news, giochi, tools, altro;
+Dataset news, giochi, tools, altro, nuovo;
 
 vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> PROGRAMMI(){
     news.link = {
@@ -26,10 +26,39 @@ vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> PROGRAMMI(
         {"tabella___check_pastiglie", cmd+"C:\\Users\\Utente\\Desktop\\salute_famiglia\\check_pastiglie\\check_pastiglie.html"},
         {"APP_java", cmd+"C:\\Users\\Utente\\Desktop\\"+nome_cartella+"\\HOME_STUDIO\\apps\\App.jar"},
     };
+    nuovo.link = {
+        {"Batch", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Batch && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Batch"},
+        {"Cpp", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Cpp"},
+        {"Java", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Java && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Java"},
+        {"Zig", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Zig && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Zig"},
+        {"NodeJS", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_NodeJS && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_NodeJS"},
+        {"Go", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Go && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Go"},
+        {"Python", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Python && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Python"},
+        {"C#", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_C# && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_C#"},
+        {"Web", 
+            "echo C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Web && "
+            "code C:\\Users\\Utente\\Desktop\\proveProgetti\\prove_Web"},
+    };
 
     vector<std::pair<string, std::pair<vector<string>, vector<Dataset>>>> Menu = {
         //{nome campo, {titoli campi}, {dataset} }
-        {"PROGRAMMI", { {"news", "giochi", "strumenti", "altro", "Apri programma","Aggiorna"}, {news, giochi, tools, altro} }}
+        {"PROGRAMMI", { {"News", "Giochi", "Strumenti", "Altro", "Nuovo"}, {news, giochi, tools, altro, nuovo} }}
     };
 
     return Menu;
